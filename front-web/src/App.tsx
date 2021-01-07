682c4eee63faf9e592f93edd76b0600751165133
Ricardo <ricardo.agnello@bol.com.br>
2021-01-07 17:52:21 -0300

    Add routes and stepsheader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './NavBar';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-     <Navbar />
-     <Home />
+     <Routes />
     </>
   );
 }
